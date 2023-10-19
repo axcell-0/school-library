803c895f72f3de00ecadc5bffe0b7a9a7ae16d7c
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-10-19 11:53:32 +0100

    Modify student.rb file

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
-    super(id, age, parent_permission, name)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯\\(ツ)/¯'
+    '¯(ツ)/¯'
+  end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.iclude?(self)
   end
 end
