50b23b903aae87af1143f6b0ce8ec10c914b5525
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-10-19 11:52:55 +0100

    Modify persone.rb file

--- persone.rb
+++ persone.rb
@@ -1,14 +1,17 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_reader :id, :name, :age
+  attr_reader :id, :name, :age, :rentals
+  attr_accessor :classroom
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = id
+    @id = rand(1_000_000)
+
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
@@ -25,5 +28,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(date, book)
+    rental = Rental.new(date, book, self)
+    rentals << rental unless rentals.include?(rental)
   end
 end
